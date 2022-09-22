f3ae4b7376448c22c8b2998530af5e1052790b43
Redemption198 <25185835+Redemption198@users.noreply.github.com>
2022-09-22 13:58:50 +0000

    Deploying to main from @ Redemption198/nurnet-plus@5f047aa80dce2bdfce80c4435aa1e4d80a496932 🚀

--- nurnet-plus/_payload.js
+++ nurnet-plus/_payload.js
@@ -1,1 +1,1 @@
-export default (function(a){return {data:{},state:{"$sdrawer-content":null,"$sdrawer-state":{currentPosition:a,setPosition:a}},prerenderedAt:1663840910280}}(""))
+export default (function(a){return {data:{},state:{"$sdrawer-content":null,"$sdrawer-state":{currentPosition:a,setPosition:a}},prerenderedAt:1663855129465}}(""))
--- nurnet-plus/poi/_payload.js
+++ nurnet-plus/poi/_payload.js
@@ -1,1 +1,1 @@
-export default (function(a){return {data:{},state:{"$sdrawer-content":null,"$sdrawer-state":{currentPosition:a,setPosition:a}},prerenderedAt:1663840910409}}(""))
+export default (function(a){return {data:{},state:{"$sdrawer-content":null,"$sdrawer-state":{currentPosition:a,setPosition:a}},prerenderedAt:1663855129567}}(""))
